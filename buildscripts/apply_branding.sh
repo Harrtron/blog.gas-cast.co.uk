72c65ad66b743512c47a9a47262ebc1576e4b4ef
Harrtron <harleyjthorne@gmail.com>
2019-05-01 13:41:51 +0100

    Add ridiculous hack to copy avatar and icon over, as it's hard coded into theme

---
+++ buildscripts/apply_branding.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Disgusting hack to overwrite horribly hard coded avatar and favicon in theme"
+cp ../branding/favicon.ico ../themes/hugo-nuo/static/favicon.ico
+cp ../branding/avatar.png ../themes/hugo-nuo/assets/images/avatar.png
