bf3d7da5d603491fb7e92773b7912ac977e7a9a0
Harrtron <harleyjthorne@gmail.com>
2019-05-01 14:10:28 +0100

    Added favicons and modified build script to hack in yet more disgusting workaround

--- buildscripts/apply_branding.sh
+++ buildscripts/apply_branding.sh
@@ -3,4 +3,4 @@ echo "Disgusting hack to overwrite horribly hard coded avatar and favicon in the
 cd buildscripts
 cp ../branding/favicon.ico ../themes/hugo-nuo/static/favicon.ico
 cp ../branding/avatar.png ../themes/hugo-nuo/assets/images/avatar.png
-
+cp ../branding/favicons/* ../themes/hugo-nuo/static/icons/
