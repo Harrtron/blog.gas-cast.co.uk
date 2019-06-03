d5ac739688a9ba2f1e5622f9d3f4c2e0d929e139
Harrtron <harley.thorne@bjss.com>
2019-06-03 11:24:21 +0100

    Switch to gas-cast.co.uk

--- buildscripts/upload_s3.sh
+++ buildscripts/upload_s3.sh
@@ -2,4 +2,4 @@
 echo "Uploading to s3"
 export AWS_ACCESS_KEY_ID=$1
 export AWS_SECRET_ACCESS_KEY=$2
-aws s3 sync public/ s3://blog.gas-cast.co.uk --delete
+aws s3 sync public/ s3://gas-cast.co.uk --delete
