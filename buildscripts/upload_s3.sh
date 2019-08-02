bd18b09d6d8942b1f3964bbd4cf4a30085919d91
Harley Thorne <harleyjthorne@gmail.com>
2019-08-02 12:24:48 +0100

    Add new website s3 bucket. Will switch to this over the next few months

--- buildscripts/upload_s3.sh
+++ buildscripts/upload_s3.sh
@@ -3,3 +3,4 @@ echo "Uploading to s3"
 export AWS_ACCESS_KEY_ID=$1
 export AWS_SECRET_ACCESS_KEY=$2
 aws s3 sync public/ s3://gas-cast.co.uk --delete
+aws s3 sync public/ s3://gascastpodcast.co.uk --delete
