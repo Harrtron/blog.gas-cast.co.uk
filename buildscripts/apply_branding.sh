d617f10fa60dd3e27702b8d306c84626c50e4cdc
Harrtron <harley.thorne@bjss.com>
2019-08-08 14:11:23 +0100

    Update to use new avatar@

--- buildscripts/apply_branding.sh
+++ buildscripts/apply_branding.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-echo "Disgusting hack to overwrite horribly hard coded avatar and favicon in theme"
+echo "Disgusting hack to overwrite horribly hard coded favicon in theme"
 cd buildscripts
 cp ../branding/favicon.ico ../themes/hugo-nuo/static/favicon.ico
-cp ../branding/avatar.png ../themes/hugo-nuo/assets/images/avatar.png
 cp ../branding/favicons/* ../themes/hugo-nuo/static/icons/
