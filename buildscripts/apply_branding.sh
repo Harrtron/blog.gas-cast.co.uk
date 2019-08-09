e44dc8dcf181a54f3fc46f018e3bcd290c688b77
Harley Thorne <harleyjthorne@gmail.com>
2019-08-09 10:28:11 +0100

    Remove hacks as using theme methods

--- buildscripts/apply_branding.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Disgusting hack to overwrite horribly hard coded favicon in theme"
-cd buildscripts
-cp ../branding/favicon.ico ../themes/hugo-nuo/static/favicon.ico
-cp ../branding/favicons/* ../themes/hugo-nuo/static/icons/
